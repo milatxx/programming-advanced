0957f72c128ac3b19785bafbe899d2acebd7ce41
milatxx <146560367+milatxx@users.noreply.github.com>
2025-01-08 20:55:28 +0100

    char-array parsing, quotes handled correctly, improved comma splitting, index-based loop

--- films/src/util/CSVReader.java
+++ films/src/util/CSVReader.java
@@ -9,20 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
-    public static List<Film> readFilmsFromCSV(String filePath){
+    public static List<Film> readFilmsFromCSV(String filePath) {
         List<Film> films = new ArrayList<>();
         String line;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             br.readLine();
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 List<String> data = parseCSVLine(line);
                 if (data.size() >= 8) {
-                    films.add(new Film(data.get(2),data.get(3),Integer.parseInt(data.get(7)),new ArrayList<>()));
+                    films.add(new Film(data.get(2), data.get(3), Integer.parseInt(data.get(7)), new ArrayList<>()));
 
                 }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.err.println("Error reading CSV file: " + e.getMessage());
         }
         return films;
@@ -33,18 +33,20 @@ public class CSVReader {
         StringBuilder currentField = new StringBuilder();
         boolean insideQuotes = false;
 
-        for (char c : line.toCharArray()){
-            if (c == '\"') {
-                insideQuotes= !insideQuotes;
-            } else if (c == ',' && !insideQuotes){
+        char[] chars = line.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char currentChar = chars[i];
+            Character nextChar = i + 1 == chars.length ? null : chars[i + 1];
+
+            if (currentChar == '"' && (nextChar != null && nextChar == '"')) {
+                insideQuotes = !insideQuotes;
+            } else if (currentChar == ',' && !insideQuotes) {
                 result.add(currentField.toString().trim());
                 currentField.setLength(0);
             } else {
-                currentField.append(c);
-
+                currentField.append(currentChar);
             }
         }
-        result.add(currentField.toString().trim());
         return result;
     }
 }
